aa369c4369f97338eb6a1db02cea7e777a2724ab
Jorge Covello <jorge.covello@rebaseit.tech>
2025-07-15 21:20:41 +0200

    Update web3froms.ts

--- src/lib/web3froms.ts
+++ src/lib/web3froms.ts
@@ -1,16 +1,14 @@
-const WEB3FORMS_URL = "https://api.web3forms.com/submit";
-const WEB3FORMS_ACCESS_KEY = import.meta.env.VITE_WEB3FORMS_ACCESS_KEY;
-
 export default async function submitEmailmConfirmation(email: string, code?: string) {
-    return await fetch(WEB3FORMS_URL, {
+    return await fetch("https://api.web3forms.com/submit", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-            access_key: WEB3FORMS_ACCESS_KEY,
+            access_key: import.meta.env.VITE_WEB3FORMS_ACCESS_KEY,
             subject: "¡Bienvenido a Turnio Early Access!",
             from_name: "Turnio",
             email,
             message: '¡Gracias por registrarte para el acceso anticipado a Turnio! Te mantendremos informado.' + (code ? `Tu código es: ${code}` : ''),
+            redirect: 'false'
         }),
     });
 }
